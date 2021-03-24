03f1ac22e941b0c824440c5d6341076c8b589a53
Mohammad Parsa <mparsa1372@gmail.com>
2021-03-25 01:49:37 +0430

    Add files via upload
    Fixed some problems in displaying the hadith, right-aligned and in one line

--- imamali.php
+++ imamali.php
@@ -1,13 +1,25 @@
 <?php
 
 /*
-Plugin Name: احادیث حضرت علی
-Plugin URI: https://rasamvira.ir
-Description: احادیث حضرت امیر المومنین علی علیه السلام - با این افزونه میتوانید احادیث مولا را در بخش های مختلف سایت نمایش دهید با استفاده از این افزونه شما میتوانید از 60 حدیث موجود در افزونه به صورت تصادفی در نواحی مختلف سایت خود استفاده کنید.
-Author: محمد پارسا
-Version: 1.0.0
-Author URI: https://mohammad-parsa.ir
-*/
+ * Plugin Name: Ahadis Imam Ali
+ * Plugin URI: https://rasamvira.ir
+ * Description: احادیث حضرت امیر المومنین علی علیه السلام - با این افزونه میتوانید احادیث مولا را در بخش های مختلف سایت نمایش دهید با استفاده از این افزونه شما میتوانید از 60 حدیث موجود در افزونه به صورت تصادفی در نواحی مختلف سایت خود استفاده کنید.
+ * Author: محمد پارسا
+ * Version: 1.0.0
+ * Author URI: https://mohammad-parsa.ir
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Domain Path: /languages
+ * Requires at least: 4.9
+ * Requires PHP: 5.2.4
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 //Directories And URL
 define('DIRIMAMALI' , plugin_dir_path(__FILE__));
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Ahadis Imam Ali ===
+Contributors: rasamvira
+Tags: Hadis, ImamAli
+Requires at least: 4.9
+Tested up to: 5.6
+Stable tag: 1.0.0
+Requires PHP: 5.2.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+افزونه احادیث امیر المومنین علی علیه السلام
+
+== Description ==
+
+احادیث حضرت امیر المومنین علی علیه السلام - با این افزونه میتوانید احادیث مولا را در بخش های مختلف سایت نمایش دهید با استفاده از این افزونه شما میتوانید از 60 حدیث موجود در افزونه به صورت تصادفی در نواحی مختلف سایت خود استفاده کنید.
+
+
+== Changelog ==
+
+= 1.0.0 =
+Initial release.
+
+== Frequently Asked Questions ==
+
+= Default settings =
+
+برای نمایش احادیث در بخش های مختلف سایت میتوانید از شرت کد زیر در برگه ها ، منو ها ، ابزارک ها ، نوشته ها استفاده نمائید در حال حاظر افزونه از 60 حدیث پشتیبانی میکند و با هر بار تازه سازی صفحه حدیث تغییر پیدا میکند.
+[imamalirasam]
+
+== Screenshots ==
